104401b68824cb890e24d04a0599112427999b36
Sven Wagener <sven@awesome.ug>
2020-09-24 13:14:13 +0200

    Its growing

--- Examples/Component/Component.php
+++ Examples/Component/Component.php
@@ -1,15 +1,13 @@
 <?php
 
-require '../vendor/autoload.php';
+require '../../vendor/autoload.php';
 
 use AWSM\LibWP\Component\Component;
 
 $component = new Component();
-
 $component->setup(
     'MyApp.php',
     'MyHooks.php',
     'MyAssets.php'
 );
-
 $component->run();
---
+++ Examples/ComponentEasy/Component.php
@@ -0,0 +1,9 @@
+<?php
+
+require '../../vendor/autoload.php';
+
+use AWSM\LibWP\Component\Component;
+
+$component = new Component();
+$component->setup();
+$component->run();
