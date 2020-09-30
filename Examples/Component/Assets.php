fc4e9cea9e481ba411b83658d4c7b458d5b232cb
Sven Wagener <sven@awesome.ug>
2020-09-30 10:43:05 +0200

    Changed to bool check parameter

--- Examples/Component/Assets.php
+++ Examples/Component/Assets.php
@@ -5,8 +5,10 @@ namespace AWSM\LibWP\Examples\Component;
 use AWSM\LibWP\WP\Assets\Assets;
 use AWSM\LibWP\WP\Assets\CSS;
 use AWSM\LibWP\WP\Assets\JS;
+use AWSM\LibWP\WP\Core\Location;
+use Locale;
 
 Assets::instance()
-    ->add( new JS( 'Assets/Dist/JS/admin.js',     array( Location::class, 'isBackend' ) ) )
-    ->add( new JS( 'Assets/Dist/JS/index.js',     array( Location::class, 'isFrontend' ) ) )
-    ->add( new CSS( 'Assets/Dist/CSS/style.css',  array( Location::class, 'isFrontend' ) ) );
+    ->add( new JS( 'Assets/Dist/JS/admin.js' ),    Location::admin() )
+    ->add( new JS( 'Assets/Dist/JS/index.js' ),    Location::frontend() )
+    ->add( new CSS( 'Assets/Dist/CSS/style.css' ), Location::frontend() );
