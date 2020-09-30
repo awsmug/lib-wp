e001653a6e4c68ea13cabcb73231ad63aaea068c
Sven Wagener <sven@awesome.ug>
2020-09-30 12:50:43 +0200

    Added exceptions

---
+++ src/WP/Core/CoreException.php
@@ -0,0 +1,16 @@
+<?php
+
+<?php
+
+namespace AWSM\LibWP\WP;
+
+use AWSM\LibWP\WP\WPException;
+
+/**
+ * WP lib exception wrapper
+ * 
+ * @since 1.0.0
+ */
+class CoreException extends WPException {
+
+}
---
+++ src/WP/WPException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AWSM\LibWP\WP;
+
+use Exception;
+
+/**
+ * WP lib exception wrapper
+ * 
+ * @since 1.0.0
+ */
+class WPException extends Exception {
+
+}
