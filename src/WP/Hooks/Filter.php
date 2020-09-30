073097dfae644f11a3c6ece29aa57f39603dc416
Sven Wagener <sven@awesome.ug>
2020-09-30 10:41:43 +0200

    Fixed wrong variable name

--- src/WP/Hooks/Filter.php
+++ src/WP/Hooks/Filter.php
@@ -14,13 +14,13 @@ class Filter extends Hook {
      * @param string $tag Hook tag
      * @param array  $callback Callback
      * @param int    $priority Priority of hook
-     * @param int    $accepted_args Number of accepted arguments.
+     * @param int    $acceptedArgs Number of accepted arguments.
      * 
      * @since 1.0.0
      */
-    public function __construct( string $tag, array $callback, int $priority = 10, $accepted_args = 1 )
+    public function __construct( string $tag, array $callback, int $priority = 10, $acceptedArgs = 1 )
     {
         $this->type = 'filter';
-        parent::__construct( $tag, $callback, $priority, $accepted_args );
+        parent::__construct( $tag, $callback, $priority, $acceptedArgs );
     }
 }
