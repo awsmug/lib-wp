d545ba1af9cf53f7f519f3cba6d7bad77de08954
Sven Wagener <sven@awesome.ug>
2020-10-06 17:03:29 +0200

    Added ComponentException

---
+++ src/Component/ComponentException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AWSM\LibWP\Component;
+
+use AWSM\LibWP\WP\WPException;
+
+/**
+ * WP lib component exception wrapper
+ * 
+ * @since 1.0.0
+ */
+class ComponentException extends WPException {
+
+}
