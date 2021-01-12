c79dc46f26f647e5a5db95c88de107ca45e6d773
Sven Wagener <sven@awesome.ug>
2021-01-12 16:34:53 +0100

    Updated example

--- src/Examples/Plugin/Components/HelloWorld/HelloWold.php
+++ src/Examples/Plugin/Components/HelloWorld/HelloWold.php
@@ -5,6 +5,7 @@ namespace AWSM\LibWP\Examples\Plugin\Components\HelloWorld;
 require '../../vendor/autoload.php';
 
 use AWSM\LibWP\Component\Component;
+use AWSM\LibWP\WP\Hooks\Hooks;
 
 /**
  * Hello World Component
@@ -12,4 +13,18 @@ use AWSM\LibWP\Component\Component;
  * @since 1.0.0
  */
 class HelloWorld Extends Component {
+
+    public function __construct()
+    {   
+        Hooks::instance()->assign( $this );
+    }
+
+    /**
+     * Filter title.
+     * 
+     * @since 1.0.0
+     */
+    public function filterTitle() {
+        return 'This is my title';
+    }
 }
--- src/Examples/Plugin/Components/HelloWorld/Hooks.php
+++ src/Examples/Plugin/Components/HelloWorld/Hooks.php
@@ -7,5 +7,4 @@ use AWSM\LibWP\WP\Hooks\Action;
 use AWSM\LibWP\WP\Hooks\Hooks;
 
 Hooks::instance()
-    ->add( new Action( 'wp_head',   array( Main::class, 'logo' ) ) )
-    ->add( new Action( 'wp_footer', array( Main::class, 'address' ) ) );
+    ->add( new Action( 'wp_title',   array( HelloWorld::class, 'filterTitle' ) ) );
