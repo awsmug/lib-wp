ea5a6d953bf2aeb1160776027c15ea082f3881d8
Sven Wagener <sven@awesome.ug>
2021-01-14 13:57:05 +0100

    Added exception location

--- src/WP/ExceptionCatcher.php
+++ src/WP/ExceptionCatcher.php
@@ -2,6 +2,7 @@
 
 namespace AWSM\LibWP\WP;
 
+use AWSM\LibTools\Callbacks\CallerDetective;
 use AWSM\LibWP\WP\Core\AdminNotices;
 use AWSM\LibWP\WP\Core\Location;
 
@@ -21,6 +22,8 @@ class ExceptionCatcher {
      * @since 1.0.0
      */
     public static function error( string $message ) {
+        $message = sprintf( 'Error: %s (%s)', $message, self::exceptionLocation() );
+
         if( Location::admin() ) {
             AdminNotices::instance()->add( $message, 'error' );
         }
@@ -34,6 +37,8 @@ class ExceptionCatcher {
      * @since 1.0.0
      */
     public static function warning( string $message ) {
+        $message = sprintf( 'Warning: %s (%s)', $message, self::exceptionLocation() );
+
         if( Location::admin() ) {
             AdminNotices::instance()->add( $message, 'warning' );
         }
@@ -47,6 +52,8 @@ class ExceptionCatcher {
      * @since 1.0.0
      */
     public static function notice( string $message ) {
+        $message = sprintf( 'Notice: %s (%s)', $message, self::exceptionLocation() );
+
         if( Location::admin() ) {
             AdminNotices::instance()->add( $message, 'notice' );
         }
@@ -60,8 +67,25 @@ class ExceptionCatcher {
      * @since 1.0.0
      */
     public static function info( string $message ) {
+        $message = sprintf( 'Info: %s (%s)', $message, self::exceptionLocation() );
+
         if( Location::admin() ) {
             AdminNotices::instance()->add( $message, 'info' );
         }
+    }
+
+    /**
+     * Get exception location.
+     * 
+     * @return string Description of location where exception happened.
+     * 
+     * @since 1.0.0
+     */
+    private static function exceptionLocation() {
+        return sprintf ( 'in class "%s->%s of file "%s"',
+            CallerDetective::detect(2)->className(),
+            CallerDetective::detect(2)->functionName(),
+            CallerDetective::detect(2)->file()
+        );
     }
 }
