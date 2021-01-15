a85b9011f9a925c40e4735859d14d003cc9c72be
Sven Wagener <sven@awesome.ug>
2021-01-15 13:55:08 +0100

    Using dirname

--- src/WP/Core/PluginInfo.php
+++ src/WP/Core/PluginInfo.php
@@ -240,6 +240,6 @@ class PluginInfo {
      * @since 1.0.0
      */
     public function getPath() {
-        return plugin_dir_path( $this->pluginFile->path() );
+        return dirname( $this->pluginFile->path() );
     }
 }
