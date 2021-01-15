8a80748cf80068bc41df628492f5ad87b6715e8e
Sven Wagener <sven@awesome.ug>
2021-01-15 11:40:52 +0100

    Removed variables for Hooks and Assets constructor

--- src/Examples/Plugin/Components/HelloWorld/Assets.php
+++ src/Examples/Plugin/Components/HelloWorld/Assets.php
@@ -7,7 +7,7 @@ use AWSM\LibWP\WP\Assets\CSS;
 use AWSM\LibWP\WP\Assets\JS;
 use AWSM\LibWP\WP\Core\LocationCallbacks;
 
-(new Assets( $variables['plugin'] ) )
+( new Assets() )
     ->add( new JS( 'Assets/Dist/JS/admin.js' ),    LocationCallbacks::admin() )
     ->add( new JS( 'Assets/Dist/JS/index.js' ),    LocationCallbacks::frontend() )
     ->add( new CSS( 'Assets/Dist/CSS/style.css' ), LocationCallbacks::frontend() );
--- src/Examples/Plugin/Components/HelloWorld/Hooks.php
+++ src/Examples/Plugin/Components/HelloWorld/Hooks.php
@@ -5,5 +5,5 @@ namespace AWSM\LibWP\Examples\Plugin\Components\HelloWorld;
 use AWSM\LibWP\WP\Hooks\Action;
 use AWSM\LibWP\WP\Hooks\Hooks;
 
-(new Hooks( $variables['plugin'] ) )
+(new Hooks() )
     ->add( new Action( 'wp_title',   array( HelloWorld::class, 'filterTitle' ) ) );
