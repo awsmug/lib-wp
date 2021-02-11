abcf6cfa380e284820e3099c1dd26d63a82aa122
Sven Wagener <sven@awesome.ug>
2021-02-11 21:01:48 +0100

    Using function, not var

--- Examples/Plugin/Components/HelloWorld/Hooks.php
+++ Examples/Plugin/Components/HelloWorld/Hooks.php
@@ -5,5 +5,5 @@ namespace AWSM\LibWP\Examples\Plugin\Components\HelloWorld;
 use AWSM\LibWP\Examples\Plugin\MyPlugin;
 use AWSM\LibWP\WP\Hooks\Action;
 
-MyPlugin::instance()->hooks
+MyPlugin::instance()->hooks()
     ->add( new Action( 'wp_title',   array( HelloWorld::class, 'filterTitle' ) ) );
