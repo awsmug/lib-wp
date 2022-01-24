df7fdc1f307335cdf30ba205d85acb99cd0acd24
Sven Wagener <sven@awesome.ug>
2022-01-24 09:42:34 +0100

    Removed empty test

--- Tests/PluginInfoTest.php
+++ Tests/PluginInfoTest.php
@@ -5,17 +5,17 @@ use PHPUnit\Framework\TestCase;
 
 final class PluginInfoTest extends TestCase {
 	public function testFunction(): void {
-        $pi = new PluginInfo( dirname( __FILE__ ) . '/Assets/Plugin.php' );
+                $pi = new PluginInfo( dirname( __FILE__ ) . '/Assets/Plugin.php' );
 
-        $this->assertEquals('My Plugin', $pi->getName() );
-        $this->assertEquals('A wonderfull plugin.', $pi->getDescription() );
-        $this->assertEquals('Sven Wagener', $pi->getAuthor() );
-        $this->assertEquals('https://sven-wagener.com', $pi->getAuthorURI() );
-        $this->assertEquals('1.2.3', $pi->getVersion() );
-        $this->assertEquals('myplgin', $pi->getTextDomain() );
-        $this->assertEquals('/assets/langauges/', $pi->getDomainPath() );
-        $this->assertTrue( $pi->getNetwork() );
-        $this->assertEquals('5.4.0', $pi->getRequiredWPVersion() );
-        $this->assertEquals('7.1.0', $pi->getRequiredPHPVersion() );
+                $this->assertEquals('My Plugin', $pi->getName() );
+                $this->assertEquals('A wonderfull plugin.', $pi->getDescription() );
+                $this->assertEquals('Sven Wagener', $pi->getAuthor() );
+                $this->assertEquals('https://sven-wagener.com', $pi->getAuthorURI() );
+                $this->assertEquals('1.2.3', $pi->getVersion() );
+                $this->assertEquals('myplgin', $pi->getTextDomain() );
+                $this->assertEquals('/assets/langauges/', $pi->getDomainPath() );
+                $this->assertTrue( $pi->getNetwork() );
+                $this->assertEquals('5.4.0', $pi->getRequiredWPVersion() );
+                $this->assertEquals('7.1.0', $pi->getRequiredPHPVersion() );
 	}
 }
